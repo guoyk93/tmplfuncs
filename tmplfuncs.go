40e7fa8876750180a9153e3102131a1fc8c148d0
Guo Y.K <hi@guoyk.net>
2020-07-06 15:02:03 +0800

    Update tmplfuncs.go
    add functions

--- tmplfuncs.go
+++ tmplfuncs.go
@@ -4,31 +4,48 @@ import (
 	"errors"
 	"net"
 	"os"
+	"os/user"
 	"strconv"
 	"strings"
 )
 
 var Funcs = map[string]interface{}{
 	// built-in functions
-	"netResolveIPAddr":  net.ResolveIPAddr,
-	"osHostname":        os.Hostname,
-	"osUserCacheDir":    os.UserCacheDir,
-	"osUserConfigDir":   os.UserConfigDir,
-	"osUserHomeDir":     os.UserHomeDir,
-	"stringsContains":   strings.Contains,
-	"stringsFields":     strings.Fields,
-	"stringsIndex":      strings.Index,
-	"stringsLastIndex":  strings.LastIndex,
-	"stringsHasPrefix":  strings.HasPrefix,
-	"stringsHasSuffix":  strings.HasSuffix,
-	"stringsRepeat":     strings.Repeat,
-	"stringsReplaceAll": strings.ReplaceAll,
-	"stringsSplit":      strings.Split,
-	"stringsToLower":    strings.ToLower,
-	"stringsToUpper":    strings.ToUpper,
-	"stringsTrimPrefix": strings.TrimPrefix,
-	"stringsTrimSpace":  strings.TrimSpace,
-	"stringsTrimSuffix": strings.TrimSuffix,
+	"netResolveIPAddr":    net.ResolveIPAddr,
+	"osHostname":          os.Hostname,
+	"osUserCacheDir":      os.UserCacheDir,
+	"osUserConfigDir":     os.UserConfigDir,
+	"osUserHomeDir":       os.UserHomeDir,
+	"osGetegid":           os.Getegid,
+	"osGetenv":            os.Getenv,
+	"osGeteuid":           os.Geteuid,
+	"osGetgid":            os.Getgid,
+	"osGetgroups":         os.Getgroups,
+	"osGetpagesize":       os.Getpagesize,
+	"osGetpid":            os.Getpid,
+	"osGetppid":           os.Getppid,
+	"osGetuid":            os.Getuid,
+	"osGetwd":             os.Getwd,
+	"osTempDir":           os.TempDir,
+	"osUserLookupGroup":   user.LookupGroup,
+	"osUserLookupGroupId": user.LookupGroupId,
+	"osUserCurrent":       user.Current,
+	"osUserLookup":        user.Lookup,
+	"osUserLookupId":      user.LookupId,
+	"stringsContains":     strings.Contains,
+	"stringsFields":       strings.Fields,
+	"stringsIndex":        strings.Index,
+	"stringsLastIndex":    strings.LastIndex,
+	"stringsHasPrefix":    strings.HasPrefix,
+	"stringsHasSuffix":    strings.HasSuffix,
+	"stringsRepeat":       strings.Repeat,
+	"stringsReplaceAll":   strings.ReplaceAll,
+	"stringsSplit":        strings.Split,
+	"stringsToLower":      strings.ToLower,
+	"stringsToUpper":      strings.ToUpper,
+	"stringsTrimPrefix":   strings.TrimPrefix,
+	"stringsTrimSpace":    strings.TrimSpace,
+	"stringsTrimSuffix":   strings.TrimSuffix,
 
 	// extra functions
 	"k8sStatefulSetID": k8sStatefulSetID,
